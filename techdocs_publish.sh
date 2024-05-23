c0fffa4c9e1fe32e54618a40fd9bd73a81a0464b
davlloyd <davidlloyd0@gmail.com>
2024-05-23 21:38:21 +1000

    updated

--- techdocs_publish.sh
+++ techdocs_publish.sh
@@ -2,6 +2,9 @@
 
 # Script to generate and publish techdopcs from current directory to gcs
 export GOOGLE_APPLICATION_CREDENTIALS=~/gcr.json
+export AWS_ACCESS_KEY_ID=Oku06bnS0KwCVdlOX0Wo
+export AWS_SECRET_ACCESS_KEY=igllRkqg81iL4O43SgaGtYGQq17ZzTAoXwMkJOQ1
+export AWS_REGION=home-sydney
 techdocs-cli generate --source-dir . 
-techdocs-cli publish --publisher-type googleGcs --storage-name techdocs-tap --entity alpha/Component/surfersapi
+techdocs-cli publish --publisher-type awsS3 --storage-name techdocs --entity alpha/Component/surfersapi --awsEndpoint http://minio.home.local:9000
 
